7ae628c312c856180806cd6f7925251d55cec686
Alexander CS Hendorf <hendorf@gmail.com>
2024-06-14 09:19:05 +0200

    updated documentation

--- src/preprocess_attendees.py
+++ src/preprocess_attendees.py
@@ -1,31 +1,10 @@
-import base64
-import hashlib
 from collections.abc import Callable
 from pathlib import Path
 
 import pandas as pd
-from pydantic import UUID4, BaseModel, EmailStr
 
 from src import logger
-
-
-class Attendee(BaseModel):
-    full_name: str
-    first_name: str
-    email: EmailStr
-    ticket_reference: str
-    attended_how: str
-    hash: str | None = None
-    uuid: str | None = None
-
-    def model_post_init(self, ctx):  # noqa: ARG002
-        # short hash to identify the attendee
-        hash_this = ''.join([x.upper() for x in self.full_name if x.isalnum()]) + self.ticket_reference.strip()
-        hsh = hashlib.sha512()
-        hsh.update(hash_this.encode("utf-8"))
-        self.hash = base64.urlsafe_b64encode(hsh.hexdigest().encode("utf-8"))[:6].decode("utf-8").upper()
-        # uuid for webservice
-        self.uuid = str(UUID4(hsh.hexdigest()[:32]))
+from src.models.attendee import Attendee
 
 
 class Attendees:
